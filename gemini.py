c56d42eb41b488ab3f7c935e3b88e306f6a7471a
Ilyasse <sidki.imane10@gmail.com>
2024-04-29 21:39:54 +0100

    refactor: add main

--- gemini.py
+++ gemini.py
@@ -4,53 +4,55 @@ from dotenv import load_dotenv
 import json
 import os
 
-load_dotenv()
-genai.configure(api_key = os.getenv("api_token"))
+def extract_json_from_image(image_path) :
+    try:
+        img = PIL.Image.open(os.path.join(os.getcwd(), "factures_images", image_path)) 
+        if img.mode != 'RGB':
+            img = img.convert('RGB')
+    except FileNotFoundError as e:
+        return(json.dumps({"status": False, "message": "Failed to open image " + str(e)}, indent=4))
+    try:
+        with open("instructions.txt", "r") as file:
+            instructions = file.read()
+    except FileNotFoundError:
+        return(json.dumps({"status": False, "message": "Failed to read instructions file"}, indent=4))
 
-try:
-    img = PIL.Image.open(os.path.join(os.getcwd(), r"factures_images", r"Facture Janv 2024 (1)_page-0002.jpg")) 
-    # img = img.resize((700, int(img.height * 700 / img.width))) it's a bad idea to resize the image as it may affect the model's performance to identify the content of the image
-    if img.mode != 'RGB':
-        img = img.convert('RGB')
-except FileNotFoundError as e:
-    print(json.dumps({"status": False, "message": "Failed to open image " + str(e)}, indent=4))
-    exit()
+    # Set up the model
+    generation_config = {
+    "temperature": 0,
+    "max_output_tokens": 2048,
+    }
 
-try:
-    with open("instructions.txt", "r") as file:
-        instructions = file.read()
-except FileNotFoundError:
-    print(json.dumps({"status": False, "message": "Failed to read instructions file"}, indent=4))
-    exit()
+    model = genai.GenerativeModel(model_name="gemini-pro-vision", generation_config=generation_config)
 
-# Set up the model
-generation_config = {
-  "temperature": 0,
-  "max_output_tokens": 2048,
-}
+    response = model.generate_content([instructions, img], stream=False)
+    response.resolve()
 
-model = genai.GenerativeModel(model_name="gemini-pro-vision", generation_config=generation_config)
+    result = response.text
 
-response = model.generate_content([instructions, img], stream=False)
-response.resolve()
+    # trim the result to remove the leading and trailing whitespaces
+    result = result.strip()
 
-result = response.text
+    # check if the result contains ``` or ```json then replace them with empty string
+    if "```json" in result:
+        result = result.replace("```json", "")
+    if "```" in result:
+        result = result.replace("```", "")
 
-# trim the result to remove the leading and trailing whitespaces
-result = result.strip()
+    try:
+        result = json.loads(result)
+        pretty_result = json.dumps(result, indent=4)
+    except json.JSONDecodeError:
+        return("Failed to decode JSON from response. Response was:" + result)
+    except Exception as e:
+        return(f"An error occurred: {e}")
 
-# check if the result contains ``` or ```json then replace them with empty string
-if "```json" in result:
-    result = result.replace("```json", "")
-if "```" in result:
-    result = result.replace("```", "")
+    return pretty_result
 
-try:
-    result = json.loads(result)
-    pretty_result = json.dumps(result, indent=4)
-    print(pretty_result)
-except json.JSONDecodeError:
-    print("Failed to decode JSON from response. Response was:")
-    print(result)
-except Exception as e:
-    print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    load_dotenv()
+    genai.configure(api_key = os.getenv("api_token"))
+    image_path = r"Facture Janv 2024 (1)_page-0002.jpg"
+    json_data = extract_json_from_image(image_path)
+    print(json_data)
