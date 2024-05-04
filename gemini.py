ddf82b0718ae667421706192e343fadcaaa2e470
imane-sidki <sidki.imane10@gmail.com>
2024-05-04 20:41:56 +0100

    chore:changes

--- gemini.py
+++ gemini.py
@@ -21,15 +21,20 @@ def extract_json_from_image(image_path) :
     # Set up the model
     generation_config = {
     "temperature": 0,  # no imagination
-    "max_output_tokens": 2048,
+    "max_output_tokens": 2500,
     }
 
     model = genai.GenerativeModel(model_name="gemini-pro-vision", generation_config=generation_config)
 
     response = model.generate_content([instructions, img], stream=False)
     response.resolve()
-
-    result = response.text
+    if response.parts:
+        result = response.text
+    else:
+        # Handle the case where no valid part is returned
+        print("No valid content was generated.")
+        print("Safety Ratings:", response.candidate.safety_ratings)
+        return(json.dumps({"status": False, "message": "Safety Ratings: "+response.candidate.safety_ratings}, indent=4))
 
     # trim the result to remove the leading and trailing whitespaces
     result = result.strip()
@@ -51,34 +56,34 @@ def extract_json_from_image(image_path) :
     return pretty_result
 
 
-def prepare_finetuning_dataset(output_folder, subset_name):
-    images_folder = "factures_images"
-
+def prepare_finetuning_dataset(subset_name):
+    output_folder = "finetuning_dataset"
     # Define image subfolder within output folder
     subset_folder = os.path.join(output_folder, subset_name)
     image_subfolder = os.path.join(output_folder, 'images')
   
-    if not os.path.exists(image_subfolder):
-        os.makedirs(image_subfolder)
     if not os.path.exists(subset_folder):
         os.makedirs(subset_folder)
     # Initialize list to hold all JSON data
     all_json_data = []
-
-    for filename in os.listdir(images_folder):
+    counter = 0
+    for filename in os.listdir(image_subfolder):
+        if counter == 59:
+            break
         if filename.endswith(".jpg") or filename.endswith(".jpeg") or filename.endswith(".png"):
             # Rename image
-            image_path = os.path.join(images_folder, filename)
+            image_path = os.path.join(image_subfolder, filename)
             unique_id = str(uuid.uuid4())  # Generate unique ID for each image
             new_image_name = f"{unique_id}.jpg"  # Rename image with unique ID
             os.rename(image_path, os.path.join(image_subfolder, new_image_name))
             image_path = os.path.join(image_subfolder, new_image_name)
+            counter += 1
             gemini_json_response = extract_json_from_image(image_path)
-            prompt_instruction = "Extract in json format only, and without further explanation, the following information from the invoice image: company sender, recipient, total, invoice date, due date."
+            prompt_instruction = "Extract in json format information from the invoice image."
             # Structure for LLaVA JSON
             json_data = {
                 "id": unique_id,
-                "image": new_image_name,
+                "image": image_path,
                 "conversations": [
                     {
                         "from": "human",
@@ -94,20 +99,14 @@ def prepare_finetuning_dataset(output_folder, subset_name):
             all_json_data.append(json_data)
 
     # Save all_json_data to a JSON file to generate dataset for finetuning
-    json_output_path = os.path.join(output_folder, subset_name, "dataset.json")
+    json_output_path = os.path.join(subset_folder, "dataset.json")
     with open(json_output_path, "w") as json_file:
         json.dump(all_json_data, json_file, indent=4)
 
-if __name__ == "__main__":
+if __name__ == "__main__":-7=+4850
     load_dotenv()
     # Configure gemini api key
     genai.configure(api_key = os.getenv("api_token"))
-
-    # image_path = os.path.join(os.getcwd(), "factures_images", r"Facture Janv 2024 (1)_page-0002.jpg")
-    # json_data = extract_json_from_image(image_path)
-    # print(json_data)
-
     # Extracting json data from all invoices
-    output_folder = "finetuning_dataset/"
     subset_name = "train"
-    prepare_finetuning_dataset(output_folder, subset_name)
+    prepare_finetuning_dataset(subset_name)
